029f42f54b21c058d7382660aa2e03019711d034
rogerrm95 <rogerrm@live.com>
2022-01-18 22:47:07 -0300

    Authentication and Authorization #3

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import cors from 'cors'
 import routes from './routes.js'
 
 const app = express()
+const PORT = process.env.PORT
 
 app.use(bodyParser.json())
 app.use(cors())
+
+app.use(express.json({ extended: false }))
+
 app.use(routes)
 
-app.listen(8080, () => console.log('Running'))
+app.listen(PORT | 8080, () => console.log('Running'))
