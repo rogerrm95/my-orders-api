ee810d26ae69adc65819496296a7f8ace1e91446
rogerrm95 <rogerrm@live.com>
2022-03-29 19:55:34 -0300

    Add: Rota patch (Atualização de um item do menu)

--- src/routes/foods.js
+++ src/routes/foods.js
@@ -23,14 +23,7 @@ Foods.get('/foods', async (req, res, next) => {
 
         if (!foodsDB) return []
 
-        const foods = foodsDB.data.map(food => {
-            return {
-                ...food.data,
-                id: food.ref.value.id
-            }
-        })
-
-        return res.status(200).json(foods)
+        return res.status(200).json(foodsDB)
     } catch {
         return res.status(500).json({ message: 'Erro interno, tente novamente' })
     }
@@ -45,6 +38,36 @@ Foods.post('/foods', async (req, res, next) => {
                 q.Collection('foods'),
                 { data }
             )
+        )
+
+        return res.status(201).json(response)
+    } catch {
+        res.status(500).json({ message: 'Erro interno, tente novamente' })
+    }
+
+})
+
+Foods.patch('/foods', async (req, res, next) => {
+    const data = req.body
+    const { id } = req.body
+
+    try {
+        if (!id) {
+            return res.status(404).json({ message: 'Item não encontrado!' })
+        }
+
+        const response = await Faunadb.query(
+            q.Update(
+                q.Select("ref",
+                    q.Get(
+                        q.Match(
+                            q.Index("food_by_id"), id
+                        )
+                    )
+                ),
+                {
+                    data
+                })
         )
 
         return res.status(201).json(response)
