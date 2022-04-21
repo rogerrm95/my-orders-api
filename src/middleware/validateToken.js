34b28f5b29bf43142a37b9e73df9c7a532c67995
rogerrm95 <rogerrm@live.com>
2022-04-21 17:58:10 -0300

    Update: Change status code #403 message

--- src/middleware/validateToken.js
+++ src/middleware/validateToken.js
@@ -3,20 +3,20 @@ import jwt from 'jsonwebtoken';
 function validate(req, res, next) {
     try {
         const { authorization } = req.headers
-        
-        if (!authorization) return res.status(403).send('Token ausente')
-        
+
+        if (!authorization) return res.status(403).json({ message: 'Token ausente' })
+
         const tokenEncrypted = authorization.split([" "])[1]
         const decodedToken = jwt.verify(tokenEncrypted, process.env.AUTH_SECRET_KEY, (err, decodedToken) => {
-            if (err) return res.status(403).json({message: 'Token inválido'})
-            
+            if (err) return res.status(403).json({ message: 'Token inválido' })
+
             return decodedToken
         })
-        
+
         const now = Math.floor(Date.now() / 1000)
 
-        if (now > decodedToken.exp) return res.status(403).json({message: 'Token expirado'})
-    
+        if (now > decodedToken.exp) return res.status(403).json({ message: 'Token expirado' })
+
         return next();
     } catch {
         res.status(500).send('Erro durante o processamento')
